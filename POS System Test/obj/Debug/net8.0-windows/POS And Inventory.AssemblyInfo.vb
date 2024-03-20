73ee9688c9aef0875f3005490aa0d368fbc38c3a
Charyd Caccam <caccam.2000@gmail.com>
2024-03-20 17:53:32 +0800

    Changes

--- POS System Test/obj/Debug/net8.0-windows/POS And Inventory.AssemblyInfo.vb
+++ POS System Test/obj/Debug/net8.0-windows/POS And Inventory.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("POS And Inventory"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b569a1fe280440e833bbd43b8a66c1a60bdcdf3"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d37967ad6715195c844310fd115734c21f82da"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("POS And Inventory"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("POS And Inventory"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
