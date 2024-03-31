3ff44f72b82bc230682ba0423b40dff9d5be5286
Ktrippings <caccam.2000@gmail.com>
2024-03-31 16:41:17 +0800

    reset

--- POS System Test/Inventory/Inventory.vb
+++ POS System Test/Inventory/Inventory.vb
@@ -2,45 +2,8 @@
 Imports System.IO
 
 Public Class Inventory
-    Private Sub Inventory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
 
-    Private Sub LoadCategories()
-
-    End Sub
-
-    Private Sub cbxCategory_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxCategory.SelectedIndexChanged
-        PopulateDataGridViewForCategory(cbxCategory.SelectedItem.ToString)
-    End Sub
-
-    Public Sub AddItemsFromAllCategories()
-
-    End Sub
-
-    Private Sub btAddCategory_Click(sender As Object, e As EventArgs) Handles btAddCategory.Click
-        StockCategoryAdd.Visible = True
-    End Sub
-
-    Private Sub btAddStock_Click(sender As Object, e As EventArgs) Handles btAddStock.Click
-        AddStock.Visible = True
-    End Sub
-
-    Private Sub btStockHistory_Click(sender As Object, e As EventArgs) Handles btStockHistory.Click
-        StockHistory.Visible = True
-    End Sub
-
-    Private Sub cbxCategory_Click(sender As Object, e As EventArgs) Handles cbxCategory.Click
-        LoadCategories()
-    End Sub
-
-    Private Sub txbxSearch_TextChanged(sender As Object, e As EventArgs) Handles txbxSearch.TextChanged
-
-    End Sub
-
-    Public Sub PopulateDataGridViewForCategory(selectedCategory As String)
-
-    End Sub
 
 End Class
 
