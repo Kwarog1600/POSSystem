bfefec8908026c999248d6abe85e7689ff9f0c30
Ktrippings <caccam.2000@gmail.com>
2024-06-11 00:40:24 +0800

    changes

--- POS System Test/obj/Release/net8.0-windows/POS And Inventory.AssemblyInfo.vb
+++ POS System Test/obj/Release/net8.0-windows/POS And Inventory.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("R3Hub Techologies Inc."),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5da647fa705d5e4097c6aab7352a847be6c0d43"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdbe89179823130836b51b3a2f917b3d125d7b2"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("POS And Inventory"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("POS And Inventory"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
